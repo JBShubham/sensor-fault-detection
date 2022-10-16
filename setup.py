9a3baf976ae1771b4de7c67312a1018e007b5980
Shubham <jbshubham.outlook.com>
2022-10-16 14:33:36 +0530

    my commit

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ setup(
     author="ineuron",
     author_email="avnish@ineuron.ai",
     packages = find_packages(),
-    install_requires=get_requirements(),#["pymongo==4.2.0"],
+    #install_requires=get_requirements(),#["pymongo==4.2.0"],
+    install_requires=["pymongo==4.2.0"],
 )
 
